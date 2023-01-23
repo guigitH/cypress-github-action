b1d7062a38dbb8bdd05a78817c56233b97927fb2
Guillaume <slack77@hotmail.fr>
2023-01-23 18:35:10 +0100

    Remove comment

--- cypress/e2e/test/network_requests.cy.js
+++ cypress/e2e/test/network_requests.cy.js
@@ -3,10 +3,7 @@ context('Network Requests', () => {
     cy.visit('/commands/network-requests')
   })
 
-  // Manage HTTP requests in your app
-
   it('cy.request() - make an XHR request', () => {
-    // https://on.cypress.io/request
     cy.request('https://jsonplaceholder.cypress.io/comments')
       .should((response) => {
         expect(response.status).to.eq(200)
